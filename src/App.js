42a910f804b54add5e8e8922264c3ac66ed47452
Timra <59473768+TimraWork@users.noreply.github.com>
2021-12-23 18:47:00 +0500

    feat: 🎸 add audio

--- src/App.js
+++ src/App.js
@@ -1,35 +1,83 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
-import questions from './questions.json';
 import logoKapitalBank from "./assets/img/logoKapitalBank.png";
 import iconFifty from "./assets/img/icon__fifty.png";
 import iconHall from "./assets/img/icon__hall_help.png";
 import logo from "./assets/img/logo.png";
 import Grid from '@mui/material/Grid';
+import Tooltip from '@mui/material/Tooltip';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import { IconButton } from '@mui/material';
+// import questions from './questions.json';
+// import UpdateIcon from '@mui/icons-material/Update';
 
 export const formatBalance = (balance) =>  balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 
-function App() {
+const MUSIC_URL = 'https://timra.ru/portfolio/audio/music.mp3';
+const STORAGE = 'react-game/playMusic';
+
+const App = () => {
   const [questions, setQuestions] = useState([]);
 
-  const onSelectChange = (e) => {
-    setQuestions(questions);
-  }
+  const [isMusicPlayed, setIsMusicPlayed] = useState(true);
+  const refMusic = useRef(null);
 
-  // useEffect(() => {
-  //   console.log('data = ', data);
-  // }, [data]); //eslint-disable-line
+  useEffect(() => {
+    if (!refMusic.current) {
+      return;
+    }
+
+    if (isMusicPlayed) {
+      refMusic.current.defaultMuted = false;
+      refMusic.current.muted = false;
+    } else {
+      refMusic.current.defaultMuted = true;
+      refMusic.current.muted = true;
+    }
+  }, [isMusicPlayed]);
+
+  useEffect(() => {
+    const getStorage = localStorage.getItem(STORAGE);
+    if (getStorage) setIsMusicPlayed(JSON.parse(getStorage || ''));
+  }, []);
+
+  const handleMusicToggleClick = () => {
+    setIsMusicPlayed(!isMusicPlayed);
+    localStorage.setItem(STORAGE, JSON.stringify(!isMusicPlayed));
+  };
 
   return (
     <div className="App">
       <div className="millionaire">
         <header className="App-header">
           <img src={logoKapitalBank} alt="KapitalBank" className="logoKapitalBank" />
+          
+          <div className="volume">
+            <IconButton size="large" onClick={handleMusicToggleClick}>
+              {isMusicPlayed ? <VolumeOffIcon /> : <VolumeUpIcon />}
+              {isMusicPlayed && <audio autoPlay loop ref={refMusic} src={MUSIC_URL} />}
+            </IconButton>
+          </div>
+          
+          {/* <div className="volume">
+            <IconButton >
+              <UpdateIcon/>
+            </IconButton>
+          </div> */}
+          
           <img src={iconFifty} alt="" className="icon_right icon_right--fifty" />
-          <img src={iconHall} alt="" className="icon_right icon_right--hall" />
+          <Tooltip title={
+            <div className="tooltip_text">
+              <div className="tooltip_text__title">Подсказочка</div>
+              <div className="tooltip_text__text">Текст подсказочки. Xороший текст!!!</div>
+            </div>
+          }>
+            <img src={iconHall} alt="" className="icon_right icon_right--hall" />
+          </Tooltip>
         </header>
         <img src={logo} alt="KapitalBank" className="millionaire__logo" />
-        <div className={`question`} onClick={onSelectChange}> Центр автомобилестроения США ? </div>
+        <div className={`question`} onClick={()=>{}}> Центр автомобилестроения США ? </div>
         <Grid container spacing={0}>
           <Grid item xs={6}> <div className="answer answer--wrong"> <span className="letter">A:&emsp;</span> Нью-Йорк </div> </Grid>
           <Grid item xs={6}> <div className="answer  answer--right"> <span className="letter">B:&emsp;</span> Портсмут </div> </Grid>
