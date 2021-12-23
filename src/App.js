96aabfba400f54c2225c0cfe1cdcc5af0a200d5f
Timra <59473768+TimraWork@users.noreply.github.com>
2021-12-23 15:55:55 +0500

    feat: 🎸 add layout for answers

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import logoKapitalBank from "./assets/img/logoKapitalBank.png";
 import iconFifty from "./assets/img/icon__fifty.png";
 import iconHall from "./assets/img/icon__hall_help.png";
 import logo from "./assets/img/logo.png";
+import Grid from '@mui/material/Grid';
 
 export const formatBalance = (balance) =>  balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 
 function App() {
-  const [data, setData] = useState([]);
+  const [questions, setQuestions] = useState([]);
 
   const onSelectChange = (e) => {
-    setData(questions);
+    setQuestions(questions);
   }
 
-  useEffect(() => {
-    console.log('data = ', data);
-  }, [data]); //eslint-disable-line
+  // useEffect(() => {
+  //   console.log('data = ', data);
+  // }, [data]); //eslint-disable-line
 
   return (
     <div className="App">
@@ -25,13 +26,17 @@ function App() {
         <img src={logoKapitalBank} alt="KapitalBank" className="logoKapitalBank" />
         <img src={iconFifty} alt="" className="icon_right icon_right--fifty" />
         <img src={iconHall} alt="" className="icon_right icon_right--hall" />
-        <div className="millionaire">
-          <img src={logo} alt="KapitalBank" className="millionaire__logo" />
-          <div className={`question`} onClick={onSelectChange}>
-
-          </div>
-        </div>
       </header>
+      <div className="millionaire">
+        <img src={logo} alt="KapitalBank" className="millionaire__logo" />
+        <div className={`question`} onClick={onSelectChange}> Центр автомобилестроения США ? </div>
+        <Grid container spacing={0}>
+          <Grid item xs={6}> <div className="answer answer--wrong"> <span className="letter">A:&emsp;</span> Нью-Йорк </div> </Grid>
+          <Grid item xs={6}> <div className="answer  answer--right"> <span className="letter">B:&emsp;</span> Портсмут </div> </Grid>
+          <Grid item xs={6}> <div className="answer"> <span className="letter">C:&emsp;</span> Детройт </div> </Grid>
+          <Grid item xs={6}> <div className="answer"> <span className="letter">D:&emsp;</span> Чикаго </div> </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
