150078666a174eb570d6fe399fc85ac6b8bc29d6
Timra <59473768+TimraWork@users.noreply.github.com>
2021-12-23 12:30:58 +0500

    feat: 🎸 add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import questions from './questions.json';
-import logo from "./assets/img/logo.svg";
+import logoKapitalBank from "./assets/img/logoKapitalBank.png";
+import iconFifty from "./assets/img/icon__fifty.png";
+import iconHall from "./assets/img/icon__hall_help.png";
+import logo from "./assets/img/logo.png";
 
 export const formatBalance = (balance) =>  balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 
@@ -19,8 +22,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="exchange_rates">
-          <img src={logo} alt="logo" className="exchange_rates__logo" />
+        <img src={logoKapitalBank} alt="KapitalBank" className="logoKapitalBank" />
+        <img src={iconFifty} alt="" className="icon_right icon_right--fifty" />
+        <img src={iconHall} alt="" className="icon_right icon_right--hall" />
+        <div className="millionaire">
+          <img src={logo} alt="KapitalBank" className="millionaire__logo" />
           <div className={`question`} onClick={onSelectChange}>
 
           </div>
