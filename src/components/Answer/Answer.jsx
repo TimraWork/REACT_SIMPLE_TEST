f5a0f0ee4dd3fa1f040b8aec84b183e7ace0f0a8
Timra <59473768+TimraWork@users.noreply.github.com>
2021-12-24 16:09:11 +0500

    feat: 🎸 implement answer logic

--- src/components/Answer/Answer.jsx
+++ src/components/Answer/Answer.jsx
@@ -2,23 +2,42 @@ import { Grid } from '@mui/material';
 import React, {useState} from 'react';
 import { ANSWERS_LETTERS } from '../../App';
 import questionsJson from '../../questions.json';
+import MUSIC_URL_RIGHT from '../../assets/audio/correct.mp3';
+import MUSIC_URL_WRONG from '../../assets/audio/wrong.mp3';
 
-const Answer = ({ idx = 0, answerResult = ""  }) => {
+const Answer = ({ idx = 0  }) => {
     const [activeAnswer, setActiveAnswer] = useState(false);
+    const [answerState, setAnswerState] = useState("");
+
     const onAnswerClick = () =>  {
-        setActiveAnswer(!activeAnswer);
+        const correctAnswer = questionsJson[0].correctAnswer;
+        const currentAnswer = Object.keys(questionsJson[0].answers[idx])[0];
+
+        if(!activeAnswer) {
+            setActiveAnswer(!activeAnswer);
+        } else {
+
+            if(correctAnswer === currentAnswer) {
+                setAnswerState("right")
+            } else {
+                setAnswerState("wrong")
+            }
+        }
     }
 
     return (
         <Grid item xs={6} >
             <div
-                className={`answer ${ activeAnswer ? '' : 'answer--hide' }`}
+                className={`answer ${ activeAnswer ? '' : 'answer--hide' } answer--${answerState}`}
                 onClick={() => onAnswerClick() }
                 tabIndex={idx + 1}
             >
                 <div className="answer__letter"> {ANSWERS_LETTERS[idx]}: &nbsp; </div>
                 <div className="answer__text"> {questionsJson[0].answers[idx][ANSWERS_LETTERS[idx]]}  </div>
             </div>
+
+            {answerState === 'right' && <audio autoPlay src={MUSIC_URL_RIGHT} />}
+            {answerState === 'wrong' && <audio autoPlay src={MUSIC_URL_WRONG} />}
 
         </Grid>
     );
